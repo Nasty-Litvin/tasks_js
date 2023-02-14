f7d05fc9a142e7ebf8bb302206f1b1aab44a0d24
nasty <nastylitvin1991liverpool@gmail.com>
2023-02-14 23:04:46 +0300

    add tasks

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
+// Task
 function calcSumm(oneNum, twoNum, more, less, same) {
-    let getSumm = oneNum + twoNum;
+  let getSumm = oneNum + twoNum;
 
-    if (getSumm > 3) {
-        more()
-    } else if (getSumm < 3) {
-        less()
+  if (getSumm > 3) {
+    more();
+  } else if (getSumm < 3) {
+    less();
+  } else {
+    same();
+  }
+}
+function showMoreMess() {
+  console.log("сумма больше 3");
+}
+function showLessMess() {
+  console.log("сумма меньше 3");
+}
+function showSameMess() {
+  console.log("сумма равна 3");
+}
+// Task
+calcSumm(1, 2, showMoreMess, showLessMess, showSameMess);
+function highAndLow(numbers) {
+  function highAndLow(numbers) {
+    let array = numbers.split(" ");
+    let arraySort = array.sort((a, b) => a - b);
+    let res = `${arraySort[arraySort.length - 1]}` + " " + `${arraySort[0]}`;
+    return res;
+  }
+}
+highAndLow("1 2 8 -2 9 25");
+// Task
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+even_or_odd(25);
+even_or_odd(20);
+// Task
+let a = ["a", "a", "c", "b"];
+let b = ["a", "a", "b", ""];
+let ar1 = ["a", "a", "b", "c"];
+let ar2 = ["a", "a", "b", "c"];
+let res = 0;
+
+function checkExam(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] == array2[i]) {
+      res += 4;
+    } else if (array2[i] == "") {
+      res += 0;
     } else {
-        same()
+      res -= 1;
     }
-
-
+  }
+  if (res < 0) {
+    return 0;
+  } else {
+    return res;
+  }
 }
-function showMoreMess () {
-    console.log('сумма больше 3');
-}
-function showLessMess () {
-    console.log('сумма меньше 3');
-}
-function showSameMess () {
-    console.log('сумма равна 3');
-}
-calcSumm(1, 2, showMoreMess, showLessMess, showSameMess)
+checkExam(ar1, ar2);
+console.log(checkExam(a, b));
